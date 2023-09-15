b31bace8bdda5464a0ded718c7a75f998f04ea2d
Vini0667 <vini04.silva09+github@gmail.com>
2023-09-15 17:04:49 -0400

    Feito a aula 7

--- src/main/java/com/vini/duck/demo/Controller/DuckController.java
+++ src/main/java/com/vini/duck/demo/Controller/DuckController.java
@@ -32,16 +32,18 @@ public class DuckController {
     @GetMapping ("/new_duck")
     public String registerPage (Model model) {
         model.addAttribute("programmingList", programmingLanguagesService.findProgrammingLanguages());
+        model.addAttribute("newDuck", new Duck());
         return "/register-duck";
     }
 
     @PostMapping("/new_duck")
-    public String newDuck (@ModelAttribute ("newDuck") @Valid Duck duck, BindingResult errors, RedirectAttributes attr) {
+    public String newDuck (@ModelAttribute ("newDuck") @Valid Duck duck, BindingResult errors, RedirectAttributes attr, Model model) {
         if (errors.hasErrors()) {
-
-            return "redirect: /new_duck";
+            model.addAttribute("programmingList", programmingLanguagesService.findProgrammingLanguages());
+            return "/register-duck";
         }
         duckService.save_duck(duck);
+        attr.addFlashAttribute("msg", "Pato salvo com sucesso");
         return "redirect:/";
     }
 }
